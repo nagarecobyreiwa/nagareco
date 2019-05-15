9947d7984ed97b43272b3862d080e8b368b08072
m-usi <um.32.jpwusi@gmail.com>
2019-05-15 03:42:08 +0000

    create model, contoller, routes about genres.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    resources :genres, only: [
+                         :index,
+                         :create,
+                         :edit,
+                         :update,
+                         :destroy
+                       ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
