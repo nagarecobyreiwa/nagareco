92597dcf24bece69d09ec867ce884afb533e07e3
otegami <a.s.takuya1026@gmail.com>
2019-05-20 10:08:47 +0000

    create Cart_item about cartItemController

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,11 +1,10 @@
 class Users::CartItemsController < ApplicationController
   def index
-    @product = cart_item_params
-
   end
 
   def create
-
+    cart_item = Cart.new(product_id: cart_item_params[:id].to_i, user_id: current_user.id, quantity: 1)
+    cart_item.save
     render "index"
   end
 
@@ -18,6 +17,6 @@ class Users::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:product).permit(:product_id)
+    params.require(:product).permit(:id)
   end
 end
