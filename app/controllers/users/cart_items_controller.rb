34e947cd8d6726ab1c579bf832e6ec745e2647e7
m-usi <um.32.jpwusi@gmail.com>
2019-05-21 07:19:10 +0000

    create cart's index

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,11 +1,12 @@
 class Users::CartItemsController < ApplicationController
   def index
+    @cart_item = current_user.carts
   end
 
   def create
-    cart_item = Cart.new(product_id: cart_item_params[:id].to_i, user_id: current_user.id, quantity: 1)
+    cart_item = Cart.find_or_create_by(product_id: params[:product][:id], user_id: current_user.id, quantity: 1)
     cart_item.save
-    render "index"
+    redirect_to users_cart_items_path
   end
 
   def update
