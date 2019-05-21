04f0af8d6477347f260e956ecf2aed31839744d0
m-usi <um.32.jpwusi@gmail.com>
2019-05-21 16:19:10 +0000

    create about cart_item's  delete

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,6 +1,5 @@
 class Users::CartItemsController < ApplicationController
   def index
-<<<<<<< HEAD
     @cart_item = current_user.carts
   end
 
@@ -8,18 +7,8 @@ class Users::CartItemsController < ApplicationController
     cart_item = Cart.find_or_create_by(product_id: params[:product][:id], user_id: current_user.id, quantity: 1)
     cart_item.save
     redirect_to users_cart_items_path
-=======
-    @cart_items = Cart.where(user_id: current_user.id)
-    binding.pry
   end
 
-  def create
-    # Edit this action by using find or create by
-    cart_item = Cart.new(product_id: cart_item_params[:id].to_i, user_id: current_user.id, quantity: 1)
-    cart_item.save
-    redirect_to action: 'index'
->>>>>>> 4c33d3f50407f6d6281789fed259113e271b82a1
-  end
 
   def update
     cart_item = Cart.find(params[:id])
@@ -28,6 +17,9 @@ class Users::CartItemsController < ApplicationController
   end
 
   def destroy
+    cart_item = Cart.find(params[:id])
+    cart_item.destroy
+    redirect_to users_cart_items_path
   end
 
 end
