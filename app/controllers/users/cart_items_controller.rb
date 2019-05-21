4c33d3f50407f6d6281789fed259113e271b82a1
otegami <a.s.takuya1026@gmail.com>
2019-05-21 10:07:01 +0000

    create index action about cart_items controller. And edit render to redirect_to about create action

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,11 +1,14 @@
 class Users::CartItemsController < ApplicationController
   def index
+    @cart_items = Cart.where(user_id: current_user.id)
+    binding.pry
   end
 
   def create
+    # Edit this action by using find or create by
     cart_item = Cart.new(product_id: cart_item_params[:id].to_i, user_id: current_user.id, quantity: 1)
     cart_item.save
-    render "index"
+    redirect_to action: 'index'
   end
 
   def update
