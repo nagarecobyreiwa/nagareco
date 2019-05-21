4b08a7b2ba5da101a08d0603387e0d0db7312514
shun-itsuki <rtfqn935@yahoo.co.jp>
2019-05-21 06:26:47 +0000

    finish_address

--- app/controllers/users/addresses_controller.rb
+++ app/controllers/users/addresses_controller.rb
@@ -1,39 +1,44 @@
 class Users::AddressesController < ApplicationController
   def index
-    @addresses = Address.where(current_user.id)
-    @user = User.find(params[:id])
+    @addresses = Address.where(user_id: current_user)
+    @user = current_user
+    # @address = Address.find(params[:id])
   end
 
   def new
     @address = Address.new
-    @address.user_id = current_user.id
   end
 
   def create
     @address = Address.new(address_params)
-    @address.user_id = current_user.id
     @address.save
-    redirect_to addreses_path
+    redirect_to users_addresses_path(current_user.id)
   end
 
   def edit
     @address = Address.find(params[:id])
-    #@address.user_id = current_user.id
   end
 
   def update
-       @address = Address.find(params[:id])
-       @address.update(address_params)
-       redirect_to addresses_path
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to users_addresses_path(current_user.id)
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to users_addresses_path(current_user.id)
   end
 
 private
   def address_params
     params.require(:address).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode,
       :address, :phone_number)
+  end
+
+  def user_params
+    params.require(:user)
   end
 
 end
