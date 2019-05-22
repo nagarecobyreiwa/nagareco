21bf6ed0bb5abbd95b439ac077a66f7870b62dc2
m-usi <um.32.jpwusi@gmail.com>
2019-05-22 04:24:28 +0000

    create cart_item's total_price

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,7 +1,12 @@
 class Users::CartItemsController < ApplicationController
   def index
-    @cart_item = current_user.carts
+    @cart_items = current_user.carts
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += cart_item.quantity * cart_item.product.price
+    end
   end
+
 
   def create
     cart_item = Cart.find_or_create_by(product_id: params[:product][:id], user_id: current_user.id, quantity: 1)
