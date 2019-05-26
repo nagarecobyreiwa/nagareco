ce9dbe33d8f43f9fdc2fee902ccdcac00d149c33
otegami <a.s.takuya1026@gmail.com>
2019-05-26 07:02:18 +0000

    create delivery update action

--- app/controllers/admins/deliveries_controller.rb
+++ app/controllers/admins/deliveries_controller.rb
@@ -2,7 +2,7 @@ class Admins::DeliveriesController < ApplicationController
   # same action controller in admin and user
   def update
     order_delivery = Order.find(params[:id])
-    order_delivery.update(delivery: order_delivery.delivery + 1 )
+    order_delivery.update(derively: order_delivery.derively + 1 )
     # create next action where do you want to go?
   end
 end
