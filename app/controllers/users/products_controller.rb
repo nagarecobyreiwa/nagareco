bed4cfadc15851c5d8ee9bc3d18cc2153dc08f18
otegami <a.s.takuya1026@gmail.com>
2019-05-30 05:11:28 +0000

    create ransack action and style in idnex

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,7 +1,7 @@
 class Users::ProductsController < ApplicationController
   def index
     @search = Product.ransack(params[:q]) #ransackメソッド推奨
-    @search_products = @search.result.page(params[:page])
+    @search_products = @search.result.page(params[:page]).reverse_order
     @products = Product.where(on_sale: true).page(params[:page]).reverse_order
   end
 
